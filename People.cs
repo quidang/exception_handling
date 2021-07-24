daa54c75794cee17a676b954e966b2cfc9145614
quidang <103585238@student.swin.edu.au>
2021-07-25 01:47:22 +1000

    beginning of task

---
+++ People.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ExceptionTask
+{
+    public class User_One
+    {   
+        public string Name {get; set;}
+        public string RoomNum {get; set;}
+        public string TimeStayed {get; set;}
+        public string DateTime = Console.ReadLine();
+    }
+    public class UserTwo
+    {
+        public string FirstName;
+        public string LastName; 
+        public string RoomNum; 
+        public int DateEntered;
+        public int TimeStayed;
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System;
+
+namespace ExceptionTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+        }
+
+
+        public void getUserDetails()
+        {
+            User_One person = new User_One();
+
+            Console.WriteLine("1. Enter the room you are attending.");
+            person.RoomNum = Console.ReadLine(); 
+            Console.WriteLine("2. Enter your name.");
+            person.Name = Console.ReadLine();
+            Console.WriteLine("3. Enter the date you attended the room.");
+            person.DateTime = Console.ReadLine();
+            Console.WriteLine("4. Enter the time stayed inside the room");
+            person.TimeStayed = Console.ReadLine();
+            Console.WriteLine("Checked in details of User One");
+            Console.WriteLine(person.Name + " " + "entered" + " " + person.RoomNum + "on this" + person.DateTime+ "and stayed for" + person.TimeStayed);
+        }
+    }
+}
